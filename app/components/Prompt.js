fdf84071f7c0bf22459f63b15d12ae838d71f6d6
Adam Back <adb33@email.arizona.edu>
2016-08-14 23:53:24 -0700

    (refactor) Prompt is now stateless component.

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -2,33 +2,32 @@ var React = require( 'react' );
 var styles = require( '../styles' );
 var PropTypes = React.PropTypes;
 
-var Prompt = React.createClass({
-  propTypes: {
-    header: PropTypes.string.isRequired,
-    onUpdateUser: PropTypes.func.isRequired,
-    onSubmitUser: PropTypes.func.isRequired,
-    username: PropTypes.string.isRequired
-  },
-  render: function() {
-    return (
-      <div style={ styles.columnFlex }>
-        <h1>{ this.props.header }</h1>
-        <form style={ styles.columnFlex } onSubmit={ this.props.onSubmitUser }>
-          <input
-            placeholder="GitHub Username"
-            type="text"
-            onChange={ this.props.onUpdateUser }
-            value={ this.props.username }
-            style={ Object.assign( {}, styles.topAndBottomMargin, styles.centerText ) }/>
-          <button
-            type="submit"
-            style={ Object.assign( {}, styles.topAndBottomMargin, styles.centerText, styles.roundGreenButton ) }>
-              Continue
-          </button>
-        </form>
-      </div>
-    );
-  }
-});
+function Prompt ( props ) {
+  return (
+    <div style={ styles.columnFlex }>
+      <h1>{ props.header }</h1>
+      <form style={ styles.columnFlex } onSubmit={ props.onSubmitUser }>
+        <input
+          placeholder="GitHub Username"
+          type="text"
+          onChange={ props.onUpdateUser }
+          value={ props.username }
+          style={ Object.assign( {}, styles.topAndBottomMargin, styles.centerText ) }/>
+        <button
+          type="submit"
+          style={ Object.assign( {}, styles.topAndBottomMargin, styles.centerText, styles.roundGreenButton ) }>
+            Continue
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Prompt.propTypes = {
+  header: PropTypes.string.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  onSubmitUser: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired
+},
 
 module.exports = Prompt;
