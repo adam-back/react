113076b2ca75591630e789079a62e3131d7440e9
Adam Back <adb33@email.arizona.edu>
2016-08-16 12:37:40 -0700

    (cleanup) Removed consoles.

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -4,17 +4,14 @@ var githubHelpers = require( '../utils/githubHelpers' );
 
 var ResultsContainer = React.createClass({
   getInitialState: function() {
-    console.log( 'getting initial state' );
     return {
       isLoading: true,
       scores: []
     };
   },
   componentDidMount: function() {
-    console.log( this.props );
     githubHelpers.battle( this.props.location.state.playersInfo )
     .then(function( scores ) {
-      console.log( scores );
       this.setState({
         scores: scores,
         isLoading: false
