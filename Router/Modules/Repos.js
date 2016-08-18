3e2dadc13d98ddfe5b9bc4dee6d88561e2251ba0
Adam Back <adb33@email.arizona.edu>
2016-08-18 16:58:09 -0700

    (feat) Added About and Repos route.

--- Router/Modules/Repos.js
+++ Router/Modules/Repos.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function() {
   return (
-    <div>
-      <div>Hello, React Router!</div>
-    </div>
+    <div>Repos</div>
   );
-};
+}
--- Router/index.js
+++ Router/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './Modules/App'
+import Repos from './Modules/Repos'
+import About from './Modules/About'
 import { Router, Route, hashHistory } from 'react-router'
 
 render((
   <Router history={ hashHistory }>
     <Route path='/' component={ App }/>
+    <Route path='/repos' component={ Repos }/>
+    <Route path='/about' component={ About }/>
   </Router>
 ), document.getElementById( 'app' ));
