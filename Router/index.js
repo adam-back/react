00328037a9e3a24918d202f750a4f0b5e9e0234e
Adam Back <adb33@email.arizona.edu>
2016-08-19 17:32:36 -0700

    (feat) Added index route.

--- Router/index.js
+++ Router/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './Modules/App'
+import Home from './Modules/Home'
 import Repos from './Modules/Repos'
 import Repo from './Modules/Repo'
 import About from './Modules/About'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 
 render((
   <Router history={ hashHistory }>
     <Route path='/' component={ App }>
+      <IndexRoute component={ Home }/>
       <Route path='/repos' component={ Repos }>
         <Route path='/repos/:userName/:repoName' component={ Repo }/>
       </Route>
