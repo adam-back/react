ac165469b8fbf610ceb4bd33775069cb2b3c2a8b
Adam Back <adb33@email.arizona.edu>
2016-08-19 17:08:12 -0700

    (feat) Webpack now dynamically creates index.html and loads css.

--- Router/webpack.config.js
+++ Router/webpack.config.js
@@ -1,14 +1,24 @@
+var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: './index.js',
 
   output: {
-    filename: 'bundle.js',
-    publicPath: ''
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
   },
-
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+      { test: /.css$/, loader: 'style-loader!css-loader' }
     ]
-  }
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
 };
