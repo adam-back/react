2a2ba490a153e43960f8d3caf9fd88e6d5b971b8
Adam Back <adb33@email.arizona.edu>
2016-08-31 22:05:27 -0700

    (feat) Added server-side routing and completed tutorial.

--- Router/index.js
+++ Router/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './Modules/App'
-import Home from './Modules/Home'
-import Repos from './Modules/Repos'
-import Repo from './Modules/Repo'
-import About from './Modules/About'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, browserHistory } from 'react-router'
+// import routes and pass them into <Router/>
+import routes from './modules/routes'
 
-render((
-  <Router history={ browserHistory }>
-    <Route path='/' component={ App }>
-      <IndexRoute component={ Home }/>
-      <Route path='/repos' component={ Repos }>
-        <Route path='/repos/:userName/:repoName' component={ Repo }/>
-      </Route>
-      <Route path='/about' component={ About }/>
-    </Route>
-  </Router>
-), document.getElementById( 'app' ));
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
