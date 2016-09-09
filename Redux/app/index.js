7cab4c9765b1b46a051470afac5286d0a9d2bbc6
Adam Back <adb33@email.arizona.edu>
2016-09-08 20:50:18 -0700

    (init) Added basic directories, dev env, and boilerplate for start of Redux project.

---
+++ Redux/app/index.js
@@ -0,0 +1,10 @@
+var React = require( 'react' );
+var ReactDOM = require( 'react-dom' );
+// var TodoApp = require( './containers/TodoApp' );
+
+
+ReactDOM.render(
+  // <TodoApp />,
+  <h1>Hello World</h1>,
+  document.getElementById( 'root' )
+);
