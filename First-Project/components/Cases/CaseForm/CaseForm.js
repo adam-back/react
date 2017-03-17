94d54b561324c19ca4cf1f6218666d2a5703c201
Adam Back <adb33@email.arizona.edu>
2017-03-17 13:49:56 -0700

    (feat) Added button to form and fixed downdown value.

--- First-Project/components/Cases/CaseForm/CaseForm.js
+++ First-Project/components/Cases/CaseForm/CaseForm.js
@@ -2,30 +2,36 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class CaseForm extends React.Component {
   state = {
-    value: null
+    value: null,
+    name: null
   };
 
-  handleSelectChange = ( event, index, value ) => {
-    console.log( 'index', index )
-    console.log( 'value', value )
-    this.setState({ value });
+  handleSelectChange = ( event, index, value ) => this.setState({ value });
+  handleNameChange = ( event, newText ) => this.setState({ name: newText });
+
+  onSubmitForm = (event) => {
+
   }
 
   render() {
+    console.log( 'staet', this.state );
     return (
       <form style={{paddingTop: 28}}>
-        <TextField hintText='Name'></TextField>
+        <TextField onChange={this.handleNameChange} hintText='Name'></TextField>
         <br/>
         <SelectField
           floatingLabelText="Employment Status"
           value={ this.state.value }
           onChange={this.handleSelectChange}>
-          <MenuItem value={1} primaryText='Employed'/>
-          <MenuItem value={2} primaryText='Unemployed'/>
+          <MenuItem value='Employed' primaryText='Employed'/>
+          <MenuItem value='Unemployed' primaryText='Unemployed'/>
         </SelectField>
+        <br/>
+        <RaisedButton label='Submit' onTouchTap={this.onSubmitForm}/>
       </form>
     );
   }
